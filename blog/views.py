1475426e67f708d3785d16d90ecf465324f43aa8
Юрій Середюк <yura2590@gmail.com>
2021-12-06 21:28:07 +0200

    add: add templates for base page and layouts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
 
-# Create your views here.
+
+def home(request):
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/pages/index.html', {'posts':posts})
